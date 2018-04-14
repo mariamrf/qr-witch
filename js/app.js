a0b55da3636c6f3135f72ab0ee813648390fb5a9
Mariam Maarouf <mrf.mariam@gmail.com>
2018-04-14 21:52:40 +0200

    SVG support.

--- js/app.js
+++ js/app.js
@@ -8,11 +8,19 @@ var app = new Vue({
 	data: {
 		image: defaults.image,
 		title: defaults.title,
-		code: new QRCode('qr-img-place', {
+		code_img: new QRCode(document.getElementById('qr-img-place'), {
 			text: 'placeholder',
 			colorLight: 'rgba(0,0,0,0)',
 			width: 2000,
-			height: 2000
+			height: 2000,
+			useSVG: false
+		}),
+		code_svg: new QRCode(document.getElementById('qr-svg-place'), {
+			text: 'placeholder',
+			colorLight: 'rgba(0,0,0,0)',
+			width: 2000,
+			height: 2000,
+			useSVG: true
 		}),
 		text: '',
 		saved: ''
@@ -21,16 +29,22 @@ var app = new Vue({
 		refresh: function (event) {
 			event.preventDefault();
 			$('.qr-img').fadeOut();
+			$('#svg_link').fadeOut();
 			this.saved = this.text;
 			if (this.empty) {
 				this.image = defaults.image;
 				this.title = defaults.title;
 			} else {
-				this.code.makeCode(this.trimmed);
+				this.code_img.makeCode(this.trimmed);
+				this.code_svg.makeCode(this.trimmed);
 				// for some reason it doesn't work directly as in the
 				// docs with Vue, so we extract the generated image.
-				this.image = this.code._el.lastElementChild.src;
+				this.image = this.code_img._el.lastElementChild.src;
 				this.title = this.trimmed;
+				exportSVG(
+					this.code_svg._el.innerHTML,
+					document.getElementById('svg_link'))
+				$('#svg_link').fadeIn();
 			}
 			$('.qr-img').fadeIn()
 		}
@@ -44,3 +58,9 @@ var app = new Vue({
 		}
 	}
 });
+
+function exportSVG(source, link_element) {
+	source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
+	var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+	link_element.href = url;
+}
